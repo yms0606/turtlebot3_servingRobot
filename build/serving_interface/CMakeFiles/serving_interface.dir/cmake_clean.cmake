dfa71f9d78941d6ddf7ebe9421ad9cd973a8dcb3
yunminsik <ymsix0622@gmail.com>
2024-11-28 17:07:15 +0900

    update fileinfo

--- build/serving_interface/CMakeFiles/serving_interface.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/serving_interface"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/serving_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/serving_node/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/yms/rokey_week4_ws/turtlebot3_servingRobot/install/serving_node'
