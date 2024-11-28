b63acede94ec9d755c955ff993db72a8aad72ff8
jeongseunghwan <hwanjung333@naver.com>
2024-11-28 17:34:58 +0900

    modify

--- build/serving_interface/serving_interface__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jsh/doosan/drive1/turtlebot3_servingRobot/build/serving_interface/serving_interface__py
-# Build directory: /home/jsh/doosan/drive1/turtlebot3_servingRobot/build/serving_interface/serving_interface__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/serving_node/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jsh/doosan/drive1/turtlebot3_servingRobot/install/serving_node'
